8b2e73688b35ae8f20564efb8acfab95d19a9ea6
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-18 16:39:31 -0400

    Print a float

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+percentage = 3.141592653589793238
+print(f'Learning Python is fun"\' - {percentage:.2f} %')
+print('Learning Python is fun"\' - {:.2f} %'.format(percentage))
+print('Learning Python is fun"\' - %.2f %%' % percentage)
