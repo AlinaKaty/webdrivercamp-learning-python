42e3e9481bea5ef85c182f6f9673e46894635c51
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-18 17:02:06 -0400

    Concat strings

---
+++ python_beginning/4_two_str_print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+webdriver_variable = "Webdriver"
+camp_variable = "Camp"
+concatenated_variable = f"Welcome to {webdriver_variable} {camp_variable}!"
+print(concatenated_variable)
