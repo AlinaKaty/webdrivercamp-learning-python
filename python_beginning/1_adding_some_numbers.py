65d7450f0f3cbf0a649797c711cae437227b9c4a
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-18 16:26:06 -0400

    Add some numbers

---
+++ python_beginning/1_adding_some_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+percentage = 100
+print(f'Learning Python is fun"\' - {percentage} %')
+print('Learning Python is fun"\' - {} %'.format(percentage))
+print('Learning Python is fun"\' - %d %%' % percentage)
