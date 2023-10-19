69c5a5a333b0ae54950ce7ed0cc011660d6757c7
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-18 20:54:19 -0400

    Print only what is needed

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+my_quote = "There should be one-- and preferably only one --obvious way to do it. lthough that way may not be obvious at first unless you're Dutch."
+words = my_quote.split()
+new_words = " ".join([ words[5], words[6], words[7], words[9], words[22] ])
+print(new_words)
