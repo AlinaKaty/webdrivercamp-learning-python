419a2776b5a719653a5e68e768c05bead0ab6764
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-23 11:54:36 -0400

    Use imported functions for calculations

---
+++ python-modules/0_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+    from calculation import add, sub, mul, div
+  
+    a = 4
+    b = 2
+  
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_sub}")
+    print(f"{a} * {b} = {result_mul}")
+    print(f"{a} / {b} = {result_div}")
