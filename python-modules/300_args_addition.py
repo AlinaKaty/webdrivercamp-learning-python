bef80c2083b836d2d6c21c9bc63859ca3ad75d27
Alina Katysheva <katysheva.alina@gmail.com>
2023-10-24 10:59:29 -0400

    Addition of all arguments

---
+++ python-modules/300_args_addition.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    args = sys.argv[1:]
+    total = 0
+
+    for arg in args:
+        total += int(arg)
+
+    print(total)
+
+if __name__ == "__main__":
+    main()
